58d16e76475cbcd724f343be3cc8e19263888ae0
Emre Akdağ <emreakdagofficial@gmail.com>
2024-12-14 19:00:14 +0300

    toolbar eklendi

--- src/Panel/test.java
+++ src/Panel/test.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class test extends JFrame {
 
@@ -31,12 +33,19 @@ public class test extends JFrame {
 	 * Create the frame.
 	 */
 	public test() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 697, 501);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("eraerwe");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel.setBounds(203, 53, 130, 198);
+		contentPane.add(lblNewLabel);
 	}
 
 }
