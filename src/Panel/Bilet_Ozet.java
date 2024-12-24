579e940ea49bf2b26cb70de9b2795799df4eec95
Emre Akdağ <emreakdagofficial@gmail.com>
2024-12-24 20:35:54 +0300

    Bilet_Özet düzenlendi

--- src/Panel/Bilet_Ozet.java
+++ src/Panel/Bilet_Ozet.java
@@ -33,7 +33,7 @@ public class Bilet_Ozet extends JFrame {
 	public Bilet_Ozet() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
